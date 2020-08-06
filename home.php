aca4b6a39d63059142b377685fb10967ab6961f6
Onésimo Metro <metrosolutionsoft@gmail.com>
2020-08-06 15:44:56 +0200

    Mapeamento dinamico do cabeçalho

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <html lang='eng' >
 
 	<head>
-		<title>Protótipo UDM | May - 2020
+		<title>Protótipo UDM | May 2020
 		</title>
 
 		<meta charset='utf-8' >
@@ -66,6 +66,9 @@
 
 		<script src='less.js' type="text/javascript" ></script>
 
+		<!-- vuejs -->
+		<script src="vuejs.js" type="text/javascript" ></script>
+
 	</head>
 
 	<body>
@@ -89,5 +92,6 @@
     	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous" ></script>
 
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous" ></script>
+
 	</body>
 </html>
